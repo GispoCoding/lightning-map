480afed31794b9b52110c4d343a45355f1a845e2
eemilhaa <eemil.haapanen@gmail.com>
2024-07-06 17:59:23 +0300

    scaffolding

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react"
+
+import loader from "./loader"
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState<any | null>(null)
+
+  useEffect(() => {
+    loader
+      .loadData()
+      .then((data) => setData(data))
+  }, [])
+
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+    </div>
+  )
+}
+
+export default App
