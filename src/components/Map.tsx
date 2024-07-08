3f82eb4cbbed618b4f68f0aa0df403afebedf17d
eemilhaa <eemil.haapanen@gmail.com>
2024-07-08 16:19:58 +0300

    add deck.gl extensions

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,7 +1,8 @@
-import { DeckGL } from "deck.gl";
+import { useState, useMemo } from "react";
+import { DeckGL, ScatterplotLayer } from "deck.gl";
+import { DataFilterExtension } from "@deck.gl/extensions";
 import Map from "react-map-gl";
 import { BASEMAP } from "@deck.gl/carto";
-import LightningLayer from "../layers";
 import Panel from "./Panel";
 
 import type { MapViewState } from "deck.gl";
@@ -22,16 +23,38 @@ const getTimeRange = (data: any): null | [minTime: number, maxTime: number] => {
 };
 
 const MapComponent = ({ data }: any) => {
+  const [filter, setFilter] = useState<[start: number, end: number] | null>(
+    null,
+  );
+  const timeRange = useMemo(() => getTimeRange(data), [data]);
+  const filterValue = filter || timeRange;
+
+  const dataFilter = new DataFilterExtension({
+    filterSize: 1,
+    fp64: false,
+  });
+
+  console.log(data);
+  console.log(timeRange);
+
+  const layers = [
+    new ScatterplotLayer({
+      id: "ScatterplotLayer",
+      data: data,
+      stroked: false,
+      getPosition: (d) => [d.longitude, d.latitude],
+      getRadius: (d) => d.peak_current,
+      getFillColor: [255, 255, 255, 200],
+      radiusScale: 5,
+      // radiusMinPixels: 0.1,
+      billboard: true,
+    }),
+  ];
   const INITIAL_VIEW_STATE: MapViewState = {
     longitude: 20,
     latitude: 65,
     zoom: 5,
   };
-
-  console.log(data);
-  console.log(getTimeRange(data));
-
-  const layers = [LightningLayer(data)];
 
   return (
     <div style={{}}>
