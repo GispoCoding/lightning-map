7a6299f4a5c0e27ad79d2e0852c82a39a078cbf1
eemilhaa <eemil.haapanen@gmail.com>
2024-07-09 15:14:24 +0300

    style infopanel

--- src/components/InfoPanel.tsx
+++ src/components/InfoPanel.tsx
@@ -1,23 +1,42 @@
-import { Paper, Typography } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 
 import formatTimeStamp from "../format-timestamp";
+
+const NormalText = ({ text }: { text: string }) => (
+  <Typography variant="body1">{text}</Typography>
+);
+
+const PrimaryHeading = ({ text }: { text: string }) => (
+  <Typography variant="h5" gutterBottom sx={{ fontWeight: "bold" }}>
+    {text}
+  </Typography>
+);
+
+const SecondaryHeading = ({ text }: { text: string }) => (
+  <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+    {text}
+  </Typography>
+);
 
 const InfoPanel = ({
   timeRange,
 }: {
   timeRange: [minTime: number, maxTime: number];
 }) => {
-  const [minTime, maxTime] = timeRange.map((t: number) => formatTimeStamp(t));
+  const [minTime, maxTime]: Array<string> = timeRange.map((t: number): string =>
+    formatTimeStamp(t),
+  );
 
   return (
-    <Paper sx={{ position: "absolute", top: "1em", padding: "10px" }}>
-      <Typography variant="h5" gutterBottom sx={{ fontWeight: "bold" }}>
-        Lightnings in Northern Europe
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        Showing every lightning strike between <br></br> {minTime} and {maxTime}
-      </Typography>
-    </Paper>
+    <Card sx={{ position: "absolute", top: "1em" }}>
+      <CardContent>
+        <PrimaryHeading text="Lightnings in Northern Europe" />
+        <NormalText text="Showing every lightning strike between" />
+        <SecondaryHeading text={minTime} />
+        <NormalText text="and" />
+        <SecondaryHeading text={maxTime} />
+      </CardContent>
+    </Card>
   );
 };
 
--- src/format-timestamp.ts
+++ src/format-timestamp.ts
@@ -1,6 +1,6 @@
 const formatTimeStamp = (timestamp: number) => {
   const date = new Date(timestamp * 1000);
-  const formatter = new Intl.DateTimeFormat("en-FI", {
+  const formatter = new Intl.DateTimeFormat("en-GB", {
     day: "2-digit",
     month: "2-digit",
     year: "numeric",
