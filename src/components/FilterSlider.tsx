2e4457eff1caf5969addef5ce92565d69350b5cb
eemilhaa <eemil.haapanen@gmail.com>
2024-07-10 15:09:24 +0300

    disable animation for now

--- src/components/FilterSlider.tsx
+++ src/components/FilterSlider.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Slider, Box, Button } from "@mui/material";
 import { PlayArrow } from "@mui/icons-material";
 import { Pause } from "@mui/icons-material";
@@ -19,36 +19,57 @@ const FilterSlider = ({
 }) => {
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const isPlayEnabled = filterRange[0] > min || filterRange[1] < max;
-  // const [newMin, setNewMin] = useState<number>()
 
-  useEffect((): any => {
-    let animation: number;
+  const enablePlay = false; // TODO
 
-    if (isPlaying) {
-      animation = requestAnimationFrame(() => {
-        const span = filterRange[1] - filterRange[0];
-        let nextValueMin = filterRange[0] + 1000; // animation speed
-        let nextValueMax = nextValueMin + span;
-        if (nextValueMax >= max) {
-          nextValueMin = min;
-          nextValueMax = nextValueMin + span;
-        }
-        setFilterRange([nextValueMin, nextValueMax]);
-        // console.log(nextValueMin);
-      });
+  // useEffect((): any => {
+  //   let animation: number;
+
+  //   if (isPlaying) {
+  //     animation = requestAnimationFrame(() => {
+  //       const span = filterRange[1] - filterRange[0];
+  //       let nextValueMin = filterRange[0] + 10; // animation speed
+  //       let nextValueMax = nextValueMin + span;
+  //       if (nextValueMax >= max) {
+  //         nextValueMin = min;
+  //         nextValueMax = nextValueMin + span;
+  //       }
+  //       setFilterRange([nextValueMin, nextValueMax]);
+  //       // console.log(nextValueMin);
+  //     });
+  //   }
+
+  //   return () => animation && cancelAnimationFrame(animation);
+  // });
+
+  const handleSliderChange = (
+    _: Event,
+    newRange: number | Array<number>,
+    activeThumb: number,
+  ) => {
+    const minDistance = 100000;
+
+    if (!Array.isArray(newRange)) {
+      return;
     }
 
-    return () => animation && cancelAnimationFrame(animation);
-  });
-
-  const handleSliderChange = (_: Event, newValue: number | number[]) => {
-    setFilterRange(newValue as number[]);
+    const distance = newRange[1] - newRange[0];
+    if (distance <= minDistance) {
+      if (activeThumb === 0) {
+        setFilterRange([newRange[0], newRange[0] + minDistance]);
+      } else {
+        setFilterRange([newRange[1] - minDistance, newRange[1]]);
+      }
+    } else {
+      setFilterRange(newRange as number[]);
+    }
   };
+
   return (
     <Box
       sx={{
         position: "absolute",
-        width: "60%",
+        width: "90%",
         bottom: "0.5em",
         // left: "5em",
         display: "flex",
@@ -56,14 +77,16 @@ const FilterSlider = ({
         transform: "translate(-50%, -50%)",
       }}
     >
-      <Button
-        color="primary"
-        disabled={!isPlayEnabled}
-        onClick={() => setIsPlaying(!isPlaying)}
-        title={isPlaying ? "Stop" : "Animate"}
-      >
-        {isPlaying ? <Pause /> : <PlayArrow />}
-      </Button>
+      {enablePlay && (
+        <Button
+          color="primary"
+          disabled={!isPlayEnabled}
+          onClick={() => setIsPlaying(!isPlaying)}
+          title={isPlaying ? "Stop" : "Animate"}
+        >
+          {isPlaying ? <Pause /> : <PlayArrow />}
+        </Button>
+      )}
       <Slider
         min={min}
         max={max}
